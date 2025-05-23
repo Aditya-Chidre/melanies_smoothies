8fb8ad4dacb98e72b66b7d06ad2b738399cc5d71
Aditya-Chidre <139644138+Aditya-Chidre@users.noreply.github.com>
2025-05-23 16:39:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write(
 name = st.text_input('Name on smoothie:')
 
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
